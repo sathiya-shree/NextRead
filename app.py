de6eb80a2e3cc35fc212486658738ec60845fbb2
sathiya-shree <159093269+sathiya-shree@users.noreply.github.com>
2025-05-25 16:06:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,23 +6,23 @@ print(df.columns.tolist())
 
 # Function to get books by author
 def get_books_by_author(author_name):
-    matching_books = df[df['Authors'].str.lower().str.contains(author_name.lower())]
+    matching_books = df[df['authors'].str.lower().str.contains(author_name.lower())]
     if not matching_books.empty:
-        return matching_books[['Title', 'Authors', 'Average rating']]
+        return matching_books[['title', 'authors', 'average rating']]
     else:
         return None
 
 def get_rating_by_title(book_title):
-    matching_books = df[df['Title'].str.lower().str.contains(book_title.lower())]
+    matching_books = df[df['title'].str.lower().str.contains(book_title.lower())]
     if not matching_books.empty:
-        return matching_books[['Title', 'Authors', 'Average rating']]
+        return matching_books[['title', 'authors', 'average rating']]
     else:
         return None
 
 # Streamlit UI
 st.title("📚 Book Info Finder")
 
-search_option = st.radio("Search by:", ['Author Name', 'Book Title'])
+search_option = st.radio("Search by:", ['authors', 'title'])
 
 if search_option == 'Author Name':
     author_input = st.text_input("Enter author name:")
@@ -34,7 +34,7 @@ if search_option == 'Author Name':
         else:
             st.warning(f"No books found for author '{author_input}'.")
 
-elif search_option == 'Book Title':
+elif search_option == 'title':
     title_input = st.text_input("Enter book title:")
     if title_input:
         results = get_rating_by_title(title_input)
